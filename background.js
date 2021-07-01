27f883d9c3014ae56cde61a40e79fb59f4a7a66d
Ulises Castro <ulisescastro2015@gmail.com>
2021-06-30 20:51:47 -0500

    Fix issue into manifest file

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.tabs.onActivated.addListener(tab => {
+  chrome.tabs.get(tab.tabId, current_tab_info => {
+    if(/^https:\/\/www\.google/.test(current_tab_info.url)) {
+      chrome.tabs.insertCSS(null, { file: '
+
+        ' })
+    }
+  })
+})
+
+
+// chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+//   console.log("injected")
+// );
