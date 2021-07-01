b4a894525277387cc3e995539d0839298756c519
Ulises Castro <ulisescastro2015@gmail.com>
2021-06-30 21:19:16 -0500

    Playing around with some methods of manifest 3

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
-chrome.tabs.onActivated.addListener(tab => {
-  chrome.tabs.get(tab.tabId, current_tab_info => {
-    if(/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      chrome.tabs.insertCSS(null, { file: '
+// NOTE: This only works with manifiest 3
+// chrome.runtime.onInstalled.addListener((reason) => {
+//   if (reason === chrome.runtime.onInstalledReason.INSTALL) {
+//     chrome.tabs.create({
+//       url: "onboarding.html",
+//     });
+//   }
+// });
+//
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    console.log(current_tab_info, "current_tab_info");
 
-        ' })
+    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+      // chrome.tabs.insertCSS(null, { file: "./mystyles.css" });
+      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+        console.log("inyected foreground.js")
+      );
     }
-  })
-})
-
+  });
+});
 
 // chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
 //   console.log("injected")
