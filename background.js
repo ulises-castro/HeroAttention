4f4416878a497e63461f09eda62a4a097856b2d4
Ulises Castro <ulisescastro2015@gmail.com>
2021-07-01 10:49:12 -0500

    Last changes into Chrome Extention version

--- background.js
+++ background.js
@@ -6,7 +6,7 @@
 //     });
 //   }
 // });
-//
+
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
     console.log(current_tab_info, "current_tab_info");
